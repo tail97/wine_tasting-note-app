36a580897a1f217e660f19b8133caf4e4a49a626
한정훈 <hanjeonghun169@gmail.com>
2023-03-06 20:49:07 +0900

    레드, 화이트, 로제 모델링

--- wine_tasting_note/scraper/models.py
+++ wine_tasting_note/scraper/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-from django.db import models
 from django.utils.timezone import now
 # from django.forms import ModelForm
 
@@ -18,6 +16,36 @@ class Wine(models.Model):
     
     def __str__(self):
         return f'{self.name}-{self.alcohol}-{self.category}-{self.price}-{self.discount}-{self.cdate}'
+    
+class White_wine(models.Model):
+    name = models.CharField(max_length=200)
+    img_url = models.CharField(max_length=200)
+    link =models.CharField(max_length=200, primary_key=True)
+    wine_info = models.CharField(max_length=200)
+    alcohol = models.CharField(max_length=200)
+    category = models.CharField(max_length = 100)
+    cdate = models.DateTimeField(default=now)
+    price = models.CharField(max_length=200)
+    discount = models.CharField(max_length=200)
+    capacity = models.CharField(max_length=200, null=True, blank=True)
+    
+    def __str__(self):
+        return f'{self.name}-{self.alcohol}-{self.category}-{self.price}-{self.discount}-{self.cdate}'
+    
+class Rose_Wine(models.Model):
+    name = models.CharField(max_length=200)
+    img_url = models.CharField(max_length=200)
+    link =models.CharField(max_length=200, primary_key=True)
+    wine_info = models.CharField(max_length=200)
+    alcohol = models.CharField(max_length=200)
+    category = models.CharField(max_length = 100)
+    cdate = models.DateTimeField(default=now)
+    price = models.CharField(max_length=200)
+    discount = models.CharField(max_length=200)
+    capacity = models.CharField(max_length=200, null=True, blank=True)
+    
+    def __str__(self):
+        return f'{self.name}-{self.alcohol}-{self.category}-{self.price}-{self.discount}-{self.cdate}'
 
 
-
+ 
