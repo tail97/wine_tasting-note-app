d98d70573af1fbe35a13c8497517b523032754f6
한정훈 <hanjeonghun169@gmail.com>
2023-03-07 18:27:50 +0900

    장고 서버에 와인 데이터 저장

--- wine_tasting_note/scraper/admin.py
+++ wine_tasting_note/scraper/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from scraper.models import Wine
+from scraper.models import White_wine
+from scraper.models import Rose_Wine
+# 장고 admin페이지에 와인데이터 저장 
+admin.site.register(Wine)
+admin.site.register(White_wine)
+admin.site.register(Rose_Wine)
