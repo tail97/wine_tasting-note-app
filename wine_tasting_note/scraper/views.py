4d4c9c08b0b6fd17d00fefcc95e2449179edd9e8
한정훈 <hanjeonghun169@gmail.com>
2023-03-07 18:30:17 +0900

    화이트 와인 views 구현

--- wine_tasting_note/scraper/views.py
+++ wine_tasting_note/scraper/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from .models import Wine
+# from django.views.generic import ListView
+from .models import White_wine , Wine
 
 # 웹 홈페이지 구현 
 def index(request):
     wine = Wine.objects.all().order_by("-cdate")
     return render(request, "index.html", {'wine':wine})
+
+def White(request):
+    whitewine = White_wine.objects.all().order_by("-cdate")
+    return render(request, 'white_wine_list.html',{'whitewine':whitewine})
