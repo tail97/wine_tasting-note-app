380b96e2155a2852b5700ee97753ed5577b10ea2
한정훈 <hanjeonghun169@gmail.com>
2023-03-08 14:47:40 +0900

    테이스팅 노트 urls 지정

--- wine_tasting_note/scraper/urls.py
+++ wine_tasting_note/scraper/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from . views import TastingListView
 
-
+app_name = 'scraper' 
 urlpatterns = [
     #웹 홈페이지 url연동
     # path('admin/', admin.site.urls),//
     path('whitewine/', views.White, name= 'white' ),
+    path('redwine/', views.Red, name= 'red' ),
+    path('rosewine/', views.Rose, name= 'rose' ),
+    path('tastingnote/', TastingListView.as_view(), name= 'tastingnote' ),
 ]
