b998d6a9c989e8ea3d745ff9fe2e9be8cd46cdcb
한정훈 <hanjeonghun169@gmail.com>
2023-03-08 14:44:06 +0900

    admin에 테이스팅 모델링 저장

--- wine_tasting_note/scraper/admin.py
+++ wine_tasting_note/scraper/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-from scraper.models import Wine
-from scraper.models import White_wine
-from scraper.models import Rose_Wine
+from scraper.models import Wine,White_wine,Rose_Wine ,Tastingnote
 # 장고 admin페이지에 와인데이터 저장 
 admin.site.register(Wine)
 admin.site.register(White_wine)
 admin.site.register(Rose_Wine)
+admin.site.register(Tastingnote)
