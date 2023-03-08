b5aed33d11a5858e9ae6f70be48ae1e8e248f4d3
한정훈 <hanjeonghun169@gmail.com>
2023-03-08 14:45:08 +0900

    테이스팅 노트 모델링

--- wine_tasting_note/scraper/models.py
+++ wine_tasting_note/scraper/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.timezone import now
+from django.contrib.auth.models import User
 # from django.forms import ModelForm
 
 class Wine(models.Model):
@@ -46,3 +47,13 @@ class Rose_Wine(models.Model):
     
     def __str__(self):
         return f'{self.name}-{self.alcohol}-{self.category}-{self.price}-{self.discount}-{self.cdate}'
+    
+
+class Tastingnote(models.Model):
+    title = models.CharField(max_length=30) #제목
+    content = models.TextField() #내용
+    created_at = models.DateTimeField(auto_now_add=True) #add 할 때의 시간
+    updated_at = models.DateTimeField(auto_now=True) #update 할 때의 시간
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/',blank = True) #이미지
+    file_upload = models.FileField(upload_to = 'blog/file/%Y/%m/%d/',blank = True ) #업로드 파일
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) #작성자
